6ba0f021c58ededad824116e4d1f9b936a2415df
Samson Mathews <samsonmmathews@gmail.com>
2025-05-28 21:09:13 -0400

    Question 2 done

---
+++ Backend_Assignment_01/Controllers/Quest02Controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Assignment_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Quest02Controller : ControllerBase
+    {
+        /// <summary>
+        /// Greets the user by name
+        /// </summary>
+        /// <param name="name">The name of the user</param>
+        /// <returns>
+        /// Returns a greeting message with the inputted name
+        /// </returns>
+        /// <example>
+        /// GET http://localhost:xx/api/q2/greeting?name=Gary
+        /// </example>
+        /// <example>
+        /// GET http://localhost:xx/api/q2/greeting?name=Ren%C3%A9e
+        /// </example>
+        [HttpGet(template: "Greeting")]
+        public string Greeting(string name)
+        {
+            return $"Hi {name}!";
+        }
+    }
+}
