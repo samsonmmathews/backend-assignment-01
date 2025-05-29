3416db732a9a60b5f95a0e835a7f8abb0845631f
Samson Mathews <samsonmmathews@gmail.com>
2025-05-28 20:38:44 -0400

    Add project files.

---
+++ Backend_Assignment_01/Controllers/Quest01Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Assignment_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Quest01Controller : ControllerBase
+    {
+        // <summary>
+        // Welcomes the user
+        // </summary>
+        // <returns>
+        // A welcome message
+        // </returns>
+        // <example>
+        // GET: api/Quest01/Welcome -> "Welcome to 5125!"
+        // </example>
+
+        [HttpGet(template: "Welcome")]
+        public string Welcome() => "Welcome to 5125!";
+    }
+}
