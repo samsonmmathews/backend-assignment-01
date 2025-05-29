6145069a8f69d20dcee6a2cc4919c58a828ffd82
Samson Mathews <samsonmmathews@gmail.com>
2025-05-28 20:57:36 -0400

    Edited the documentation

--- Backend_Assignment_01/Controllers/Quest01Controller.cs
+++ Backend_Assignment_01/Controllers/Quest01Controller.cs
@@ -7,15 +7,15 @@ namespace Backend_Assignment_01.Controllers
     [ApiController]
     public class Quest01Controller : ControllerBase
     {
-        // <summary>
-        // Welcomes the user
-        // </summary>
-        // <returns>
-        // A welcome message
-        // </returns>
-        // <example>
-        // GET: api/Quest01/Welcome -> "Welcome to 5125!"
-        // </example>
+        /// <summary>
+        /// Welcomes the user
+        /// </summary>
+        /// <returns>
+        /// A welcome message
+        /// </returns>
+        /// <example>
+        /// GET: api/Quest01/Welcome -> "Welcome to 5125!"
+        /// </example>
 
         [HttpGet(template: "Welcome")]
         public string Welcome()
