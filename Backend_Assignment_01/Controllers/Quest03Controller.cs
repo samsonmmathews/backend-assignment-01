09f63b043291a05e309c9e80c1664322f0dfbdd8
Samson Mathews <samsonmmathews@gmail.com>
2025-05-28 21:59:32 -0400

    Question 3 done

---
+++ Backend_Assignment_01/Controllers/Quest03Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Assignment_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Quest03Controller : ControllerBase
+    {
+        /// <summary>
+        /// Displays the cube of an integer
+        /// </summary>
+        /// <param name="number">The input number for which the cube needs to be found</param>
+        /// <returns>
+        /// Returns the cube of the integer
+        /// </returns>
+        /// <example>
+        /// GET api/Quest03/Cube?number=4
+        /// </example>
+        /// <example>
+        /// GET api/Quest03/Cube?number=-4
+        /// </example>
+        /// /// <example>
+        /// GET api/Quest03/Cube?number=10
+        /// </example>
+        [HttpGet(template: "Cube")]
+        public string Cube(int number)
+        {
+            return (number * number * number).ToString();
+        }
+    }
+}
