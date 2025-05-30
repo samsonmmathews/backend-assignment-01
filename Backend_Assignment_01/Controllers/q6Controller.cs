3f4087494a51d7368a444b4b43222a1c3cededc9
Samson Mathews <samsonmmathews@gmail.com>
2025-05-30 14:33:09 -0400

    Added sixth question

---
+++ Backend_Assignment_01/Controllers/q6Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Assignment_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class q6Controller : ControllerBase
+    {
+        /// <summary>
+        /// Returns the area of a regular hexagon with side length double {S} using the formula given
+        /// </summary>
+        /// <param name="side">The length of one side of a hexagon</param>
+        /// <returns>
+        /// Returns the area of the hexagon
+        /// </returns>
+        /// <example>
+        /// GET api/q6/hexagon?side=1
+        /// </example>
+        /// /// <example>
+        /// GET api/q6/hexagon?side=1.5
+        /// </example>
+        /// /// <example>
+        /// GET api/q6/hexagon?side=20
+        /// </example>
+        [HttpGet(template: "hexagon")]
+        public double Area(double side)
+        {
+            return ((3 * (Math.Sqrt(3) / 2)) * (Math.Pow(side, 2)));
+        }
+    }
+}
