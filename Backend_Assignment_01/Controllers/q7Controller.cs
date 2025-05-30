f272aba04dcf2ec386e591a67cb5c81bf952dc10
Samson Mathews <samsonmmathews@gmail.com>
2025-05-30 15:47:15 -0400

    Question 7 done

---
+++ Backend_Assignment_01/Controllers/q7Controller.cs
@@ -0,0 +1,34 @@
+﻿    using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Assignment_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class q7Controller : ControllerBase
+    {
+        /// <summary>
+        /// Returns a string representation of the current date (formatted yyyy-MM-dd), adjusted by {days}
+        /// </summary>
+        /// <param name="days">The number of days that needs to be added to today</param>
+        /// <returns>
+        /// Returns the date after adding the input number to today's date
+        /// </returns>
+        /// <example>
+        /// GET api/q7/timemachine?days=1
+        /// </example>
+        /// /// <example>
+        /// GET api/q7/timemachine?days=-1
+        /// </example>
+        /// /// <example>
+        /// GET api/q7/timemachine?days=4
+        /// </example>
+        [HttpGet(template: "timemachine")]
+        public string TimeMachine(int days)
+        {
+            DateTime calcDate = DateTime.Today;
+            DateTime answer = calcDate.AddDays(days);
+            return answer.ToString("yyyy-MM-dd");
+        }
+    }
+}
